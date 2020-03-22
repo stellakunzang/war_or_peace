91365fe7e725596ea7796ecdb3b54f6c7f7a3ad9
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-22 17:21:03 -0600

    Changed rank_of_card_at method to return 0 if the rank was nil

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,7 +7,7 @@ class Deck
 
   def rank_of_card_at(index)
     if @cards[index] == nil
-      @cards = []
+      rank = 0
     else
       @cards[index].rank
     end
@@ -27,12 +27,15 @@ class Deck
     (( high_ranking_cards.length.to_f / @cards.length.to_f) * 100.00).round(2)
   end
 
-  def remove_card
-    @cards.shift
+  def remove_card(number = 1)
+    @cards.shift(number)
   end
 
   def add_card(card)
     @cards << card
   end
 
+  def count_cards
+    @cards.length
+  end
 end
